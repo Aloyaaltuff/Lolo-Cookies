66d1cff4ed8102d6ceb6ea6d9db3d772e882d41c
aloyaaltuff <140615532+Aloyaaltuff@users.noreply.github.com>
2024-09-19 22:45:57 +0400

    Update console.py

--- console.py
+++ console.py
@@ -3,93 +3,77 @@
 import argparse
 import json
 import os
-import getpass
-from basemode import register_user, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from basemode import authenticate
+from models.user import User
+from app import db  # Import the database instance
 
-COOKIES_FILE = 'cookies.json'
+USERS_FILE = 'users.json'
 
-def load_cookies():
-    """Load cookies from a JSON file."""
-    if os.path.exists(COOKIES_FILE):
-        with open(COOKIES_FILE, 'r') as f:
+def load_users():
+    """Load users from a JSON file."""
+    if os.path.exists(USERS_FILE):
+        with open(USERS_FILE, 'r') as f:
             return json.load(f)
-    return []
+    return {}
 
-def save_cookies(cookies):
-    """Save cookies to a JSON file."""
-    with open(COOKIES_FILE, 'w') as f:
-        json.dump(cookies, f, indent=4)
+def save_users(users):
+    """Save users to a JSON file."""
+    with open(USERS_FILE, 'w') as f:
+        json.dump(users, f, indent=4)
 
-def add_cookie(args):
-    """Add a new cookie."""
-    cookies = load_cookies()
-    if args.cookie not in cookies:
-        cookies.append(args.cookie)
-        save_cookies(cookies)
-        print(f'Added new cookie: {args.cookie}')
+def register_user(username, password):
+    """Register a new user."""
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        print("Username already exists. Please choose a different username.")
+        return False
+
+    hashed_password = generate_password_hash(password, method='sha256')
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    print(f"User '{username}' registered successfully.")
+    return True
+
+def login_user(username, password):
+    """Login a user."""
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        print(f"Login successful! Welcome, {username}.")
+        return True
     else:
-        print(f'Cookie "{args.cookie}" already exists.')
-
-def delete_cookie(args):
-    """Delete a cookie."""
-    cookies = load_cookies()
-    if args.cookie in cookies:
-        cookies.remove(args.cookie)
-        save_cookies(cookies)
-        print(f'Deleted cookie: {args.cookie}')
-    else:
-        print(f'Cookie "{args.cookie}" not found.')
-
-def list_cookies(args):
-    """List all cookies."""
-    cookies = load_cookies()
-    if cookies:
-        print('Cookies:')
-        for cookie in cookies:
-            print(f'- {cookie}')
-    else:
-        print('No cookies found.')
+        print("Invalid username or password. Please try again.")
+        return False
 
 def main():
     """Main function to parse arguments and route commands."""
-    parser = argparse.ArgumentParser(description='Lolo Cookies Command Interpreter')
+    parser = argparse.ArgumentParser(description='Lolo Cookies Command Line Interface')
     subparsers = parser.add_subparsers(dest='command')
 
-    # Authentication commands
-    auth_parser = subparsers.add_parser('auth', help='Authentication commands')
-    auth_subparsers = auth_parser.add_subparsers(dest='auth_command')
-
     # Register command
-    register_parser = auth_subparsers.add_parser('register', help='Register a new user')
+    register_parser = subparsers.add_parser('register', help='Register a new user')
+    register_parser.add_argument('username', type=str, help='Username for registration')
+    register_parser.add_argument('password', type=str, help='Password for registration')
     register_parser.set_defaults(func=register_user)
 
     # Login command
-    login_parser = auth_subparsers.add_parser('login', help='Log in a user')
+    login_parser = subparsers.add_parser('login', help='Log in a user')
+    login_parser.add_argument('username', type=str, help='Username for login')
+    login_parser.add_argument('password', type=str, help='Password for login')
     login_parser.set_defaults(func=login_user)
-
-    # Add command
-    add_parser = subparsers.add_parser('add', help='Add a new cookie')
-    add_parser.add_argument('cookie', type=str, help='Name of the cookie to add')
-    add_parser.set_defaults(func=add_cookie)
-
-    # Delete command
-    delete_parser = subparsers.add_parser('delete', help='Delete a cookie')
-    delete_parser.add_argument('cookie', type=str, help='Name of the cookie to delete')
-    delete_parser.set_defaults(func=delete_cookie)
-
-    # List command
-    list_parser = subparsers.add_parser('list', help='List all cookies')
-    list_parser.set_defaults(func=list_cookies)
 
     args = parser.parse_args()
 
-    if args.command == 'auth' and args.auth_command:
-        args.func()
-    elif args.command:
-        if login_user():
-            args.func(args)
+    if args.command:
+        if args.command == 'register':
+            success = args.func(args.username, args.password)
+            if not success:
+                print("Registration failed.")
+        elif args.command == 'login':
+            args.func(args.username, args.password)
         else:
-            print('Authentication required.')
+            parser.print_help()
     else:
         parser.print_help()
 
